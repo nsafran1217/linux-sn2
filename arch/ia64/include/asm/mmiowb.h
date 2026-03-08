38e6837f19823b21e5cc6b52f4275daf3dce5562
Nathan <nsafran1217@gmail.com>
2026-03-07 20:13:06 -0500

    0004-ia64 hooks

--- arch/ia64/include/asm/mmiowb.h
+++ arch/ia64/include/asm/mmiowb.h
@@ -10,7 +10,12 @@
  * following the barrier will arrive after all previous writes.  For most
  * ia64 platforms, this is a simple 'mf.a' instruction.
  */
+#ifdef CONFIG_IA64_SGI_SN2
+extern void __sn_mmiowb(void);
+#define mmiowb()	__sn_mmiowb()
+#else
 #define mmiowb()	ia64_mfa()
+#endif
 
 #include <asm-generic/mmiowb.h>
 
