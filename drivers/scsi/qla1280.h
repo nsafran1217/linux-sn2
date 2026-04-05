0642ddc56a7ad2ed72abe89e8e2e696fa1384d93
Nathan <nsafran1217@gmail.com>
2026-04-05 10:34:51 -0400

    Change ifdefs to CONFIG_IA64_SGI_SN2

--- drivers/scsi/qla1280.h
+++ drivers/scsi/qla1280.h
@@ -1056,7 +1056,7 @@ struct scsi_qla_host {
 		uint32_t reset_active:1;		/* 3 */
 		uint32_t abort_isp_active:1;		/* 4 */
 		uint32_t disable_risc_code_load:1;	/* 5 */
-#ifdef __ia64__
+#ifdef CONFIG_IA64_SGI_SN2
 		uint32_t use_pci_vchannel:1;
 #endif
 	} flags;
