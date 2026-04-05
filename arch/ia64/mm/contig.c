a89131f10a9baa1b2aaf91b8105863ce6c1fe940
Nathan <nsafran1217@gmail.com>
2026-04-04 22:18:56 -0400

    Apply SN2 patch, change screen_info to sysfb

--- arch/ia64/mm/contig.c
+++ arch/ia64/mm/contig.c
@@ -188,10 +188,12 @@ static void __init verify_gap_absence(void)
 
 void __init arch_zone_limits_init(unsigned long *max_zone_pfns)
 {
+#ifdef CONFIG_ZONE_DMA32
 	unsigned long max_dma;
 
 	max_dma = virt_to_phys((void *) MAX_DMA_ADDRESS) >> PAGE_SHIFT;
 	max_zone_pfns[ZONE_DMA32] = max_dma;
+#endif
 	max_zone_pfns[ZONE_NORMAL] = max_low_pfn;
 
 	verify_gap_absence();
